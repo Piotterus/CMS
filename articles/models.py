fcdadec2f84436b8b1aaaef2c8b1c7a5e873305f
Piotr_Marcin Katański <piotr_marcin.katanski@student.put.poznan.pl>
2020-02-26 22:50:19 +0100

    finish1

--- articles/models.py
+++ articles/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Article(models.Model):
     title = models.CharField(max_length = 100)
     slug = models.SlugField()
+    intro = models.TextField(max_length = 250, default=None)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add = True)
     thumb = models.ImageField(default='default.png', blank=True)
@@ -11,7 +12,7 @@ class Article(models.Model):
     # add in author later
 
     def snippet(self):
-        return self.body[:50] + '...'
+        return self.intro + '</br>read more...'
 
     def __str__(self):
         return self.title
