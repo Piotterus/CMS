c81bc8bd4f9329ec907ca4bb74716b3e38bce113
Piotr_Marcin Katański <piotr_marcin.katanski@student.put.poznan.pl>
2020-02-26 02:56:44 +0100

    29

--- CMS/urls.py
+++ CMS/urls.py
@@ -19,13 +19,14 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from articles import views as articleViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
     path('accounts/', include('accounts.urls')),
     path('about/', views.about),
-    path('', views.homepage)
+    path('', articleViews.articleList, name="home")
 ]
 
 urlpatterns += staticfiles_urlpatterns()
